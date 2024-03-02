2b086ae465fc644db5a0ab2ae74841f70677b719
Bayashat <baiashat2002@gmail.com>
2024-03-03 02:43:20 +0500

    add: ModifyUser & UserResponse

--- app/api/serializers/users.py
+++ app/api/serializers/users.py
@@ -9,3 +9,18 @@ class CreateUser(BaseModel):
     password: str
     name: str
     city: str
+    
+class ModifyUser(BaseModel):
+    username: EmailStr | None = None
+    phone: PhoneNumber | None = None
+    password: str | None = None
+    name: str | None = None
+    city: str | None = None
+    
+class UserReponse(BaseModel):
+    id: int
+    username: EmailStr
+    phone: str
+    password: str
+    name: str
+    city: str
