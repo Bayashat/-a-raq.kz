41cab249d2e30d8ca1e9fe3014dd0d56bf35d4b9
Bayashat <baiashat2002@gmail.com>
2024-03-03 03:28:25 +0500

    add: Ads table Model

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.db.base import Base
 
 class IdMixin:
@@ -13,3 +15,22 @@ class User(Base, IdMixin):
     password = Column(String, nullable=False)
     name = Column(String, nullable=False)
     city = Column(String, nullable=False)
+    
+    ads = relationship("Ad", back_populates="user")
+
+class Ad(Base, IdMixin):
+    __tablename__ = "ads"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(String, index=True, nullable=False)
+    price = Column(Integer, nullable=False)
+    address = Column(String, index=True, nullable=False)
+    area = Column(Float, nullable=False)
+    rooms_count = Column(Integer, nullable=False)
+    description = Column(Text, nullable=False)
+    
+    # Foreign key to user
+    user_id = Column(Integer, ForeignKey('users.id'))
+    
+    # Define foreign key relationships
+    user = relationship("User", back_populates="ads")
