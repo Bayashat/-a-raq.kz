2f711d3e9452fb1d9af7d476f72711c6d81efd0f
Bayashat <baiashat2002@gmail.com>
2024-03-04 17:53:52 +0600

    add: GET shanyraks/{id}/ API

--- app/api/views/shanyraks.py
+++ app/api/views/shanyraks.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.api.repositories.ads import AdRepository
-from app.api.serializers.ads import CreateAd
+from app.api.serializers.ads import CreateAd, AdResponse
 from .auth import oath2_scheme, decode_jwt, get_db
 
 router = APIRouter()
@@ -20,5 +20,14 @@ def post_ad(
     ad_id = ad_repository.create_ad(db, user_id, ad)
     return {"id": str(ad_id)}
     
-    
-    
+
+@router.get("/{id}")
+def get_ad(
+    id: int,
+    token: str = Depends(oath2_scheme),
+    db: Session = Depends(get_db)
+):
+    user_id = decode_jwt(token)
+    db_ad = ad_repository.get_ad(db, user_id)
+
+    return AdResponse.model_validate(db_ad.__dict__)
