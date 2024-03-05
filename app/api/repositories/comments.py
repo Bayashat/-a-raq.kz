e80404ead5768d46795f3e09deaa703c710493f9
Bayashat <baiashat2002@gmail.com>
2024-03-05 13:28:20 +0500

    add: create_comment repo method

---
+++ app/api/repositories/comments.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import Session
+
+from app.db.models import Comment, User, Ad
+from app.api.serializers.comments import AddComment
+from fastapi import HTTPException
+
+
+
+class CommentRepository:
+    @staticmethod
+    def create_ad(db: Session, user_id: int, ad_id: int, comment_data: AddComment):
+        # Query User and Ad
+        user = db.query(User).filter(User.id == user_id).first()
+        ad = db.query(Ad).filter(Ad.id == ad_id).first()
+        
+        if not user or not ad:
+            return HTTPException(status_code=404, detail="Not found such User or Ad")
+        
+        # Create Comment
+        new_comment = Comment(
+            content = comment_data.content,
+            user_id = user_id,
+            ad_id = ad_id
+        )
+        
+        # add comment to db
+        db.add(new_comment)
+        db.commit()
+        db.refresh(new_comment)
