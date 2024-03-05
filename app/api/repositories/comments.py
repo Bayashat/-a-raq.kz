c85ab86b3609afbca10df75acfffcfc2b051bcd7
Bayashat <baiashat2002@gmail.com>
2024-03-05 15:33:19 +0500

    add: update_comment repo method

--- app/api/repositories/comments.py
+++ app/api/repositories/comments.py
@@ -28,7 +28,22 @@ class CommentRepository:
         db.refresh(new_comment)
         
     @staticmethod
-    def get_comment(db: Session, user_id: int, ad_id: int) -> list[Comment]:
+    def get_comment(db: Session, ad_id: int) -> list[Comment]:
+        # Query Ad
+        ad = db.query(Ad).filter(Ad.id == ad_id).first()
+        
+        if not ad:
+            raise HTTPException(status_code=404, detail="Not found such User or Ad")
+
+        comments = db.query(Comment).filter(
+            Comment.ad_id == ad_id
+        ).all()
+        
+        return comments
+        
+    
+    @staticmethod
+    def update_comment(db: Session, user_id: int, ad_id: int, comment_id: int, comment: AddComment):
         # Query User and Ad
         user = db.query(User).filter(User.id == user_id).first()
         ad = db.query(Ad).filter(Ad.id == ad_id).first()
@@ -36,11 +51,15 @@ class CommentRepository:
         if not user or not ad:
             raise HTTPException(status_code=404, detail="Not found such User or Ad")
 
-        comments = db.query(Comment).filter(
+        db_comment = db.query(Comment).filter(
             Comment.user_id == user_id,
-            Comment.ad_id == ad_id
-        ).all()
+            Comment.ad_id == ad_id,
+            Comment.id == comment_id
+            ).first()
         
-        return comments
+        if db_comment is None:
+            raise HTTPException(status_code=404, detail="Not found available comment for this User")
         
-    
+        db_comment.content = comment.content
+        db.commit()
+        db.refresh(db_comment)
