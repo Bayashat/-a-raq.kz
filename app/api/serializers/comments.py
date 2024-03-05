b5038894db5df1f4c6bd7c2ae56064317824baa8
Bayashat <baiashat2002@gmail.com>
2024-03-05 14:34:31 +0500

    add: CommentResponse & CommentListResponse serializers

--- app/api/serializers/comments.py
+++ app/api/serializers/comments.py
@@ -1,5 +1,16 @@
 from pydantic import BaseModel
-
+from typing import List
 
 class AddComment(BaseModel):
     content: str
+    
+
+class CommentResponse(BaseModel):
+    id: int
+    content: str
+    user_id: int
+    ad_id: int
+    
+
+class CommentListResponse(BaseModel):
+    comments: List[CommentResponse]
